bba5d19f9cb6d688f92aaa2b127758df516054ec
brianmagicr <brianmagicr@users.noreply.github.com>
2023-06-06 11:12:41 -0400

    Update sensor.py

--- custom_components/gtfs_rt/sensor.py
+++ custom_components/gtfs_rt/sensor.py
@@ -22,14 +22,12 @@ ATTR_DUE_AT = "Due at"
 ATTR_NEXT_UP = "Next train"
 ATTR_NEXT_UP_DUE_IN = "Next train due in"
 
-CONF_API_KEY = 'api_key'
-CONF_APIKEY = 'apikey'
 CONF_X_API_KEY = 'x_api_key'
 CONF_STOP_ID = 'stopid'
 CONF_ROUTE = 'route'
 CONF_DEPARTURES = 'departures'
 CONF_TRIP_UPDATE_URL = 'trip_update_url'
-CONF_VEHICLE_POSITION_URL = 'vehicle_position_url'
+CONF_DIRECTION = 'direction'
 
 DEFAULT_NAME = 'Next Train'
 ICON = 'mdi:train'
@@ -40,10 +38,8 @@ TIME_STR_FORMAT = "%H:%M"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_TRIP_UPDATE_URL): cv.string,
-    vol.Optional(CONF_API_KEY): cv.string,
     vol.Optional(CONF_X_API_KEY): cv.string,
-    vol.Optional(CONF_APIKEY): cv.string,
-    vol.Optional(CONF_VEHICLE_POSITION_URL): cv.string,
+    vol.Optional(CONF_DIRECTION): cv.string,
     vol.Optional(CONF_DEPARTURES): [{
         vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
         vol.Required(CONF_STOP_ID): cv.string,
@@ -70,7 +66,7 @@ def due_in_minutes(timestamp):
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Get the Dublin public transport sensor."""
-    data = PublicTransportData(config.get(CONF_TRIP_UPDATE_URL), config.get(CONF_VEHICLE_POSITION_URL), config.get(CONF_API_KEY), config.get(CONF_X_API_KEY), config.get(CONF_APIKEY))
+    data = PublicTransportData(config.get(CONF_TRIP_UPDATE_URL), config.get(CONF_X_API_KEY))
     sensors = []
     for departure in config.get(CONF_DEPARTURES):
         sensors.append(PublicTransportSensor(
